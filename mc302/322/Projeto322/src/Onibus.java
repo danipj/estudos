56c20190f82bfa9fd6c3a2680cf78cb47c9bf622
Daniela Palumbo <daniela.palumbo@outlook.com>
2017-11-14 14:49:48 -0200

    Updates on MC322 project

--- mc302/322/Projeto322/src/Onibus.java
+++ mc302/322/Projeto322/src/Onibus.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class Onibus {
 	
 	private int[] lugares;
@@ -10,53 +9,17 @@ public class Onibus {
 		super();
 		this.totalLugares = 40;
 		this.setQuilometragem(km);
-=======
-import java.text.SimpleDateFormat;
-import java.util.GregorianCalendar;
-
-public class Onibus {
-	private String destino;
-	private GregorianCalendar partida;
-	private int[] lugares;
-	protected int totalLugares;
-	
-	
-	public Onibus(String destino, GregorianCalendar partida) {
-		super();
-		this.destino = destino;
-		this.partida = partida;
-		this.totalLugares = 40;
->>>>>>> 255c5b41d88d652e2336f88a6412b8eab764dda3
 		lugares = new int[totalLugares];
 		//marca todos os lugares como vagos
 		for(int i=0;i<totalLugares;i++)
 			lugares[i] = 0; 
 	}
-<<<<<<< HEAD
 	
 	
 	public boolean estaVago(char poltrona, int fileira) throws LugarException{
 		if(fileira>totalLugares/4){
 			throw new LugarException("Fileira não existe");
 		}
-=======
-	public String getDestino() {
-		return destino;
-	}
-	public void setDestino(String destino) {
-		this.destino = destino;
-	}
-	public GregorianCalendar getPartida() {
-		return partida;
-	}
-	public void setPartida(GregorianCalendar partida) {
-		this.partida = partida;
-	}
-	
-	public boolean estaVago(char poltrona, int fileira){
-		if(fileira>totalLugares/4)
-			return false;
->>>>>>> 255c5b41d88d652e2336f88a6412b8eab764dda3
 		poltrona = Character.toUpperCase(poltrona);
 		switch(poltrona){
 		case 'A':
@@ -75,7 +38,6 @@ public class Onibus {
 			if(lugares[fileira*4-1]==0)
 				return true;
 			return false;
-<<<<<<< HEAD
 		default:
 			throw new LugarException("Poltrona não existe");
 		}
@@ -85,13 +47,6 @@ public class Onibus {
 		if(fileira>totalLugares/4){
 			throw new LugarException("Fileira não existe");
 		}
-=======
-		}
-		return false;
-	}
-	
-	public void reservar(char poltrona, int fileira){
->>>>>>> 255c5b41d88d652e2336f88a6412b8eab764dda3
 		poltrona = Character.toUpperCase(poltrona);
 		switch(poltrona){
 		case 'A':
@@ -106,11 +61,8 @@ public class Onibus {
 		case 'D':
 			lugares[fileira*4-1]=1;
 			break;
-<<<<<<< HEAD
 		default:
 			throw new LugarException("Poltrona não existe");
-=======
->>>>>>> 255c5b41d88d652e2336f88a6412b8eab764dda3
 		}
 	}
 	
@@ -128,7 +80,6 @@ public class Onibus {
 		return false;
 	}
 	
-<<<<<<< HEAD
 	public int getQuilometragem() {
 		return quilometragem;
 	}
@@ -140,26 +91,10 @@ public class Onibus {
 	public String toString(){
 		String r;
 		r="Poltronas:";
-=======
-	public String getPartidaFormatted(){
-	    SimpleDateFormat fmt = new SimpleDateFormat("dd-M-yyyy H:m");
-	    fmt.setCalendar(partida);
-	    String dateFormatted = fmt.format(partida.getTime());
-	    return dateFormatted;
-	}
-	
-	public String toString(){
-		String r = "Ônibus com destino a " + destino;
-		r+="\nHorário de partida: "+getPartidaFormatted();
-		r+="\nPoltronas:";
->>>>>>> 255c5b41d88d652e2336f88a6412b8eab764dda3
 		r+="\n- A B - C D";
 		for(int i=1;i<=totalLugares/4;i++)
 			r+="\n"+i+" "+lugares[(i-1)*4]+" "+lugares[i*4-3]+" - "+lugares[i*4-2]+" "+lugares[i*4-1];
 		return r;
 	}
-<<<<<<< HEAD
 	
-=======
->>>>>>> 255c5b41d88d652e2336f88a6412b8eab764dda3
 }
--- mc302/322/Projeto322/src/OnibusOvernight.java
+++ mc302/322/Projeto322/src/OnibusOvernight.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class OnibusOvernight extends Onibus{
 	private boolean servicoDeBordo;
 	private boolean televisao;
@@ -21,15 +20,5 @@ public class OnibusOvernight extends Onibus{
 	public void setServicoDeBordo(boolean servicoDeBordo) {
 		this.servicoDeBordo = servicoDeBordo;
 	}
-=======
-import java.util.GregorianCalendar;
-
-public class OnibusOvernight extends Onibus{
-
-	public OnibusOvernight(String destino, GregorianCalendar partida) {
-		super(destino, partida);
-		this.totalLugares = 20;
-	}
->>>>>>> 255c5b41d88d652e2336f88a6412b8eab764dda3
 
 }
